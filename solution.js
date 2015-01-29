442f8bf815c820d993c4045aec76350e2609b149
kulinichdenis <kulinichdenis@ukr.net>
2015-01-29 19:59:45 +0200

    tes

--- solution.js
+++ solution.js
@@ -9,6 +9,20 @@ function isEven(number) {
   // For any other number N, its evenness is the same as N - 2.
   // Define a recursive function isEven corresponding to this description. The function should accept a number parameter and return a Boolean.
   // Test it on 50 and 75. See how it behaves on -1. Why? Can you think of a way to fix this?
+	if(number < 0){
+        number = number*(-1);
+    }
+    var temp = number-2;
+    if(temp !== 0 && temp !==1){
+        return isEven(temp);
+    }else{
+        if(temp === 0){
+            return true;
+        }
+        if(temp === 1){
+            return false;
+        }
+    }
 }
 
 function pascal(row, col) {
